6849738fd8b64676f38bb2e539a3df8ce9b424d5
codeddave <drizzzydave@gmail.com>
2021-12-16 13:15:32 +0100

    play music note animation at the same time as clip path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ tl.fromTo(
   ".music-note",
   { scale: 0.5 },
   { scale: 0, opacity: 0, duration: 1 },
-  "-3"
+  "-=3"
 );
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".sub-title", { opacity: 0 }, { opacity: 1, duration: 1 });
