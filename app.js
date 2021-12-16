bbb0866031ae685221fe2e18a5612e3f0b15fc8f
codeddave <drizzzydave@gmail.com>
2021-12-16 12:47:16 +0100

    updated animation

--- app.js
+++ app.js
@@ -10,4 +10,6 @@ let tl = gsap.timeline({
 
 tl.fromTo(".page-one", { clipPath: "circle(5%)" }, { clipPath: "circle(75%)" });
 
-tl.fromTo(".intro-text", { opacity: 0 }, { opacity: 1 });
+tl.fromTo(".music-note", { scale: 0.5 }, { scale: 0, opacity: 0 });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+tl.fromTo(".sub- title", { opacity: 0 }, { opacity: 1 });
