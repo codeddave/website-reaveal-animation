83437722ba3dfdd70be5c855c6045a910438fc95
codeddave <drizzzydave@gmail.com>
2021-12-16 13:08:26 +0100

    updated animation

--- app.js
+++ app.js
@@ -12,4 +12,4 @@ tl.fromTo(".page-one", { clipPath: "circle(5%)" }, { clipPath: "circle(75%)" });
 
 tl.fromTo(".music-note", { scale: 0.5 }, { scale: 0, opacity: 0 });
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
-tl.fromTo(".sub- title", { opacity: 0 }, { opacity: 1 });
+tl.fromTo(".sub-title", { opacity: 0 }, { opacity: 1 });
